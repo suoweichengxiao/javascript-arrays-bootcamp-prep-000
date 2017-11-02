8c38e4072a8cb4409e2866497f9aa2fa6605fb75
Joe Dettmar <joedettmar@gmail.com>
2017-11-02 15:49:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(array, element) {var myArray = [1];
-var element = 'foo';
-return [element, ...myArray] }
+function addElementToBeginningOfArray(array, element) {
+return [element, ...array] }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {var awardWinningArray = [1];
 var element = 'foo';
